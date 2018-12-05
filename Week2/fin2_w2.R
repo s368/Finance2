4cc8cd51d36bc87832ea2e33854d665d071063f9
s368 <s368s368s@gmail.com>
2018-12-05 15:57:50 +0100

    Commit.

--- Week2/fin2_w2.R
+++ Week2/fin2_w2.R
@@ -7,6 +7,8 @@ b23<- -0.0022
 b31<- 0.0037
 b32<- -0.0022
 b33<- 0.0074
+
+#how this matrix (i.e. b11...b33) was calculated?
 Vmatrix<-matrix(c(b11,b12,b13,
                 + b21,b22,b23,
                 + b31,b32,b33),nrow=3,ncol=3,byrow=TRUE)
