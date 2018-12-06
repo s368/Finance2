532a9b9667c1efd3f6a5bdb0cdac9bc6ab4e64d7
s368 <s368s368s@gmail.com>
2018-12-06 13:39:36 +0100

    Update fin2_w2.R

--- Week2/fin2_w2.R
+++ Week2/fin2_w2.R
@@ -46,6 +46,15 @@ slopeSharpe_real<-(meanSharpe_real-1)/(sdSharpe * 100)
 q2_draft_real<-slopeSharpe_real * 5 + 1
 q2<-round(q2_draft_real,digits = 2)
 
+#Question_3: In the "Value-at-Risk" worksheet we list the simulated monthly returns again, and from these returns we compute the monthly rate of loss on an equally weighted portfolio of only the risky assets in column E. Use this data to estimate the Value-at-Risk at the 90% probability level.
+loss_1<-(2.1675-1.8883+3.0712)/3
+loss<-c(-1.1168,-1.3565,1.3754,-1.0396,0.5662,-0.0050,-1.9092,1.1039,-0.2332,-0.6678,-1.3045,0.8229,0.9616,-0.9685,1.0631,-2.8888,0.6022,1.1204,-0.9511,0.0810,-0.8619,0.0685,-0.2053,0.9565,0.1795,-2.4565,-0.0656,-0.1942,0.3471,-0.2564,1.2923,-0.3045,0.4619,-1.8819,-1.1397,1.9877,-0.0960,1.0440,-0.2722,-0.0218,0.8140,1.9191,2.1450,-0.3924,0.8846,-2.0569,-0.8699,-0.4551,-0.5114,-0.0412,0.2515,-0.6077,1.8807,-0.2756,-1.2639,-1.4916,-0.9395,2.3707,-0.2759,-0.7360)
+loss_sorted<-loss[order(loss)]
+p<-90/100 # 90%
+N<-60 # number of samples for 60 months (12 months in 5 years)
+q3_draft<-loss_sorted[p*N]
+q3<-round(q3_draft,digits = 2)
+
 #How to calculate covariance matrix?
 m_1<-c(2.1675,3.3875,-2.5194,-1.3959,-2.7264,0.7832,3.9749,-3.3869,-0.1714,0.5439,1.0913,-1.0361,-0.871,1.0377,-2.7236,2.7485,-0.8348,-1.2728,-0.5232,0.1174,1.1843,0.6003,-0.5785,-1.8834,1.0666,3.7147,-0.3213,1.3712,-0.7987,0.949,-2.5322,0.4613,-1.6205,4.7482,1.6551,-5.1563,-1.0738,-1.2798,-0.7793,-0.3613,0.1896,-2.6047,-3.1645,-0.6066,0.8595,3.642,0.6684,1.5556,1.4717,-2.6414,-1.6742,1.5918,-1.4625,0.0491,-0.2674,1.0214,1.0679,-5.4239,0.4125,4.9665)
 
@@ -60,7 +69,7 @@ year_return<-function(m, n_year)
   for(i in l:r)
   {
     m_full <- m_full * (1+m[i]/100)
-    print(i)
+    #print(i) # for debug only!
   }
   
   return((m_full-1)*100)
